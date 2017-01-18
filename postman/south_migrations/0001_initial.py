8c435b90c0aca833c675f3fa5cb440294757bd7a
Sergey Miletskiy <s.miletskiy@gmail.com>
2017-01-18 18:24:21 +0200

    Updated initial migration

--- postman/south_migrations/0001_initial.py
+++ postman/south_migrations/0001_initial.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
@@ -60,6 +59,48 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
+        'dacl.commodity': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Commodity'},
+            'code': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_ru': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_uk': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'unique_with': '()', 'max_length': '255', 'populate_from': "'name'", 'blank': 'True'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_ru': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_uk': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'usex_code': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'})
+        },
+        'dacl.country': {
+            'Meta': {'ordering': "['name']", 'unique_together': "(['code', 'region'],)", 'object_name': 'Country'},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '2', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'iso_3166': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '2', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_ru': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'name_uk': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'region_countries'", 'to': "orm['dacl.Region']"}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'unique_with': '()', 'max_length': '255', 'populate_from': "'name'", 'blank': 'True'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_ru': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_uk': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'usex_code': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'db_index': 'True', 'blank': 'True'})
+        },
+        'dacl.region': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Region'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "u''", 'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'null': 'True', 'default': "u''", 'max_length': '255', 'blank': 'True', 'unique': 'True', 'db_index': 'True'}),
+            'name_ru': ('django.db.models.fields.CharField', [], {'null': 'True', 'default': "u''", 'max_length': '255', 'blank': 'True', 'unique': 'True', 'db_index': 'True'}),
+            'name_uk': ('django.db.models.fields.CharField', [], {'null': 'True', 'default': "u''", 'max_length': '255', 'blank': 'True', 'unique': 'True', 'db_index': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'unique_with': '()', 'max_length': '255', 'populate_from': "'name'", 'blank': 'True'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_ru': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_uk': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
         u'postman.message': {
             'Meta': {'ordering': "[u'-sent_at', u'-id']", 'object_name': 'Message'},
             'body': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
@@ -82,6 +123,58 @@ class Migration(SchemaMigration):
             'subject': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'thread': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'child_messages'", 'null': 'True', 'to': u"orm['postman.Message']"})
         },
+        u'snapshot.marketsnapshotsection': {
+            'Meta': {'ordering': "[u'order']", 'object_name': 'MarketSnapshotSection'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'}),
+            'name_en': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'name_ru': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'name_uk': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '255', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'unique_with': '()', 'max_length': '255', 'populate_from': "'name'", 'blank': 'True'}),
+            'slug_en': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_ru': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug_uk': ('autoslug.fields.AutoSlugField', [], {'default': "u''", 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'users.user': {
+            'Meta': {'object_name': 'User'},
+            'about': ('redactor.fields.RedactorField', [], {'null': 'True', 'db_index': 'True'}),
+            'activity': ('django.db.models.fields.PositiveIntegerField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'broker': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'commodities': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "u'user_commodities'", 'to': "orm['dacl.Commodity']", 'blank': 'True', 'symmetrical': 'False', 'null': 'True', 'db_index': 'True'}),
+            'company': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'company_address': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'confirmation_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['dacl.Country']", 'null': 'True', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            'home_page': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['snapshot.MarketSnapshotSection']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'language': ('django.db.models.fields.CharField', [], {'default': "u'en'", 'max_length': '5'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_login_ip': ('django.db.models.fields.IPAddressField', [], {'default': "u'127.0.0.1'", 'max_length': '15', 'null': 'True', 'blank': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'linkedin': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'logo': (u'sorl.thumbnail.fields.ImageField', [], {'max_length': '100', 'null': 'True'}),
+            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'password_reset_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'promo_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'remarks': ('redactor.fields.RedactorField', [], {'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'subscribe': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
+            'trial': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'unsubscribe_code': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '75'}),
+            'uuid': ('uuidfield.fields.UUIDField', [], {'max_length': '32', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        }
     }
 
     complete_apps = ['postman']
